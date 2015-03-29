088c3cbdf115d73aef6ffe4d8e4522add7385bf7
Mandro <mandrek44@gmail.com>
2015-03-29 18:53:12 +0000

    Working...

---
+++ Workshop.vNext.TodoApp.Web/src/Workshop.vNext.TodoApp.Web/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Workshop.vNext.TodoApp.Web
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(ConfigureRoutes);
+        }
+
+        private void ConfigureRoutes(IRouteBuilder routes)
+        {
+            routes.MapRoute(
+               name: "default",
+               template: "{controller}/{action}/{id?}",
+               defaults: new { controller = "Home", action = "Index" });
+        }
+    }
+}
---
+++ Workshop.vNext.TodoApp.Web/src/Workshop.vNext.TodoApp.Web/wwwroot/app.js
@@ -0,0 +1,21 @@
+﻿var todoApp = angular.module('todoApp', []);
+
+todoApp.controller("TodoController", function ($scope) {
+    $scope.newTodoTask = "";
+    $scope.todos = [
+        { task: "Take garbage" },
+        { task: "Drive home " },
+        { task: "Find Wally" }
+    ];
+
+    $scope.markAsDone = function(todoToRemove) {
+        $scope.todos = $scope.todos.filter(function(todo) {
+            return todo.task != todoToRemove.task;
+        });
+    };
+
+    $scope.addNewTodo = function() {
+        $scope.todos.push({ task: $scope.newTodoTask });
+        $scope.newTodoTask = "";
+    };
+});
