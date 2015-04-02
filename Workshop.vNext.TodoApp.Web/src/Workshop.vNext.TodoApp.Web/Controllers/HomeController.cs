6ae492960307c80c7c3252977a89bddfbab86e95
Mandro <mandrek44@gmail.com>
2015-04-02 13:42:11 +0000

    Simple API

--- Workshop.vNext.TodoApp.Web/src/Workshop.vNext.TodoApp.Web/Controllers/HomeController.cs
+++ Workshop.vNext.TodoApp.Web/src/Workshop.vNext.TodoApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
 
 namespace Workshop.vNext.TodoApp.Web.Controllers
 {
@@ -7,6 +8,29 @@ namespace Workshop.vNext.TodoApp.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+    }
+
+	public class TodoController
+	{
+        
+        [HttpGet]  
+        [ActionName("Index")]      
+        public IEnumerable<TodoTask> GetAll()
+        {
+            return new TodoTask[]
+            {
+                new TodoTask() { Name = "test" }
+            };
+		}
+
+        public class TodoTask
+        {
+            public TodoTask()
+            {
+            }
+
+            public string Name { get; internal set; }
         }
     }
 }
